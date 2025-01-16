ce8b0fff3332ca1e270b8430d13af6a8cc181867
Luis Plata <www.luisplata@gmail.com>
2025-01-16 18:11:22 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => fake()->name,
+            'email' => fake()->unique()->safeEmail,
         ]);
 
         $this->call(UsuarioInicial::class);
